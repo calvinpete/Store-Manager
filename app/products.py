0578e54905571eaa2a183cf10fa59d1773c10690
calvinpete <tinkacalvin@gmail.com>
2018-10-18 13:53:06 +0300

    [Delivers #161243752] modify products model

--- app/products.py
+++ app/products.py
@@ -1,50 +1,57 @@
 class Product:
     """This class holds the logic for managing inventory"""
     def __init__(self):
-        self.stock = []  # A list to hold available products
+        self.stock = {}  # A data structure to hold available products
 
-    def add_product(self, product_name, category, quantity, unit_quantity, cost, price):
+    def create_category(self, category):
+        """
+        This creates a list to hold products for that category
+        :param category:
+        :return:
+        """
+        self.stock[category] = []
+
+    def add_product(self, category, product_name, quantity, cost, price):
         """
         This adds a product to the inventory
-        :param unit_quantity:
-        :param product_name:
         :param category:
+        :param product_name:
         :param quantity:
         :param cost:
         :param price:
         :return:
         """
-        product_id = len(self.stock) + 1
+        product_id = len(self.stock[category]) + 1
         product = {
             "product_id": product_id,
             "product_name": product_name,
-            "category": category,
             "quantity": quantity,
-            "unit_quantity": unit_quantity,
             "cost": cost,
             "price": price
         }
-        self.stock.append(product)
+        self.stock[category].append(product)
         return self.stock
 
-    def check_product(self, product_id):
+    def check_product(self, category, product_id):
         """
         This checks if the product exists
+        :param category:
         :param product_id:
         :return:
         """
-        for product in self.stock:
+        for product in self.stock[category]:
             if product_id == product["product_id"]:
                 return True
 
-    def get_product(self, product_id):
+    def get_product(self, category, product_id):
         """
         This fetches a product's details
+        :param category:
         :param product_id:
         :return:
         """
-        for product in self.stock:
-            if self.check_product(product_id):
+        for product in self.stock[category]:
+            if self.check_product(category, product_id):
                 return product
 
     def get_all_products(self):
@@ -53,3 +60,6 @@ class Product:
         :return:
         """
         return self.stock
+
+
+item = Product()
