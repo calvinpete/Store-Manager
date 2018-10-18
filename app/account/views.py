57a0ced819ad86fd976065fb25141af7acd1cf7e
calvinpete <tinkacalvin@gmail.com>
2018-10-18 21:54:49 +0300

    [Delivers #161244464] build login route

--- app/account/views.py
+++ app/account/views.py
@@ -1,6 +1,10 @@
+import jwt
+import datetime
+from functools import wraps
 from flask import jsonify, request
 from app import app
 from app.accounts import staff
+from instance.config import Config
 
 
 @app.errorhandler(400)
@@ -38,7 +42,7 @@ def sign_up():
             email_address=email_address,
             username=username,
             password=password):
-        return jsonify({"message": "Please a enter a string"})
+        return jsonify({"message": "Please a enter a string"}), 400
 
     if staff.check_input_validity(
             name=name,
@@ -57,3 +61,51 @@ def sign_up():
     else:
         staff.register(name, company_id, phone_number, email_address, username, password, admin=True)
         return jsonify({"message": "You've been successfully registered"}), 201
+
+
+@app.route('/store-manager/api/v1/auth/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    if len(data.keys()) != 2:
+        return jsonify({"message": "please type in the missing fields"}), 400
+
+    email_address = data['email_address']
+    password = data['password']
+
+    if not staff.check_user(email_address):
+        return jsonify({"message": "User does not exist, please register"}), 400
+
+    if not staff.check_password(password):
+        return jsonify({"message": "Invalid password, please try again"}), 400
+    else:
+        token = jwt.encode(
+            {
+                "email_address": email_address,
+                "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
+            },
+            Config.SECRET_KEY
+        )
+        return jsonify(
+            {
+                "token": token.decode("UTF-8"),
+                "message": "You have successfully logged in"
+            }
+        ), 200
+
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+        if not token:
+            return jsonify({"message": "Token is missing"}), 401
+        try:
+            data = jwt.decode(token, Config.SECRET_KEY)
+            current_user = staff.get_user(email_address=data["email_address"])
+        except jwt.DecodeError:
+            return jsonify({"message": "Token is invalid"}), 401
+        return f(current_user, *args, **kwargs)
+
+    return decorated
