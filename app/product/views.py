c7ded963d63638eee28d4cae90cd2dff1672d015
calvinpete <tinkacalvin@gmail.com>
2018-10-19 14:10:00 +0300

    [Delivers #161244249] build add product route

--- app/product/views.py
+++ app/product/views.py
@@ -28,3 +28,42 @@ def create_category(current_user):
         return jsonify({"message": "The {} Category already exists".format(category)}), 409
     item.create_category(category)
     return jsonify({"message": "{} category successfully created".format(category)}), 201
+
+
+@app.route('/store-manager/api/v1/<category>/products', methods=['POST'])
+@token_required
+def add_product(current_user, category):
+    if not staff.check_admin(current_user):
+        return jsonify({"message": "You do not have administrator access"}), 401
+
+    data = request.get_json()
+    if len(data.keys()) != 4:
+        return jsonify({"message": "You should have the product_name, quantity, details and price fields"}), 400
+
+    product_name = data['product_name']
+    quantity = data['quantity']
+    details = data['details']
+    price = data['price']
+
+    if staff.check_input_type(
+            product_name=product_name,
+            quantity=quantity,
+            details=details,
+            price=price):
+        return jsonify({"message": "Please a enter a string"}), 400
+
+    if staff.check_input_validity(
+            product_name=product_name,
+            quantity=quantity,
+            details=details,
+            price=price):
+        return jsonify({"message": "Values are required"}), 400
+
+    if not item.check_category(category):
+        return jsonify({"message": "{} category does not exist".format(category)}), 404
+
+    if not item.check_product(category, product_name, details):
+        item.add_product(category, product_name, quantity, details, price)
+        return jsonify({"message": "Product successfully added"}), 201
+    else:
+        return jsonify({"message": "{} {} already exists".format(details, product_name)}), 404
