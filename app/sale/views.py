5a80de31035cfaec5df0ef41e13be5248d0397a6
calvinpete <tinkacalvin@gmail.com>
2018-10-21 11:36:09 +0300

    [Delivers #161244144] build get all sale records route

--- app/sale/views.py
+++ app/sale/views.py
@@ -1,6 +1,7 @@
 from flask import jsonify, request
 from app import app
 from app.accounts import staff
+from app.products import item
 from app.sales import staff_sales
 from app.account.views import token_required
 
@@ -13,7 +14,7 @@ def create_sale_record(current_user):
 
     data = request.get_json()
     if len(data.keys()) != 4:
-        return jsonify({"message": "You should have the category, product_id, quantity and status fields"}), 400
+        return jsonify({"message": "You should have the category, product_id, quantity and sale_type fields"}), 400
 
     category = data['category']
     product_id = data['product_id']
@@ -22,22 +23,21 @@ def create_sale_record(current_user):
 
     if staff.check_input_type(
             category=category,
-            product_id=product_id,
-            quantity=quantity,
-            slae_type=sale_type):
+            sale_type=sale_type):
         return jsonify({"message": "Please a enter a string"}), 400
+
+    if item.check_product_input_type(product_id=product_id, quantity=quantity):
+        return jsonify({"message": "Please a enter an integer"}), 400
 
     if staff.check_input_validity(
             category=category,
-            product_id=product_id,
-            quantity=quantity,
             sale_type=sale_type):
         return jsonify({"message": "Values are required"}), 400
 
     try:
 
         if not staff_sales.check_available_product(category, product_id, quantity):
-            return jsonify({"message": "There is not enough to sale"}), 404
+            return jsonify({"message": "Invalid quantity requested"}), 404
 
         staff_sales.sale_product(category, product_id, quantity, sale_type, staff.get_user_name(current_user))
         return jsonify({"message": "Sale record successfully created"}), 201
@@ -58,3 +58,13 @@ def get_single_sale_record(current_user, sale_id):
 
     except IndexError:
         return jsonify({"message": "Sale record does not exist"}), 201
+
+
+@app.route('/store-manager/api/v1/sales', methods=['GET'])
+@token_required
+def get_all_sale_records(current_user):
+
+    if not staff.check_admin(current_user):
+        return jsonify({"message": "You do not have administrator access"}), 401
+
+    return jsonify(staff_sales.get_all_sales()), 200
