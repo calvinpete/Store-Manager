9561e1c1ba84c8381dec796f38884db7c1a7e39d
calvinpete <tinkacalvin@gmail.com>
2018-10-21 23:21:53 +0300

    [Delivers #161243594] modify AccountTestCase class

--- tests/test_accounts_model.py
+++ tests/test_accounts_model.py
@@ -18,48 +18,38 @@ class AccountTestCase(TestBase):
         This method tests the register method of the Account class
         :return:
         """
-        self.assertListEqual(
+        self.assertEqual(len(
             self.user_account.register(
                 self.test_user1["name"], self.test_user1["email_address"], self.test_user1["password"]
-            ),
-            [{"name": "Robe", "email_address": "Robe@gmail.com", "password": "R&e", "user_id": 1, "admin": False}]
-        )
+            )), 2)
 
     def test_check_user(self):
         """
         This method tests the check_user method of the Account class
         :return:
         """
-        self.user_account.register(self.test_user22["name"], self.test_user22["email_address"],
-                                   self.test_user22["password"])
-        self.assertEqual(self.user_account.check_user(self.test_user22["email_address"]), True)
+        self.assertEqual(self.user_account.check_user(self.test_user23["email_address"]), True)
 
     def test_get_user(self):
         """
         This method tests the get_user method of the Account class
         :return:
         """
-        self.user_account.register(self.test_user22["name"], self.test_user22["email_address"],
-                                   self.test_user22["password"])
-        self.assertEqual(self.user_account.get_user(self.test_user22["email_address"]), 1)
+        self.assertEqual(self.user_account.get_user(self.test_user23["email_address"]), 1)
 
     def test_get_user_name(self):
         """
         This method tests the get_user_name method of the Account class
         :return:
         """
-        self.user_account.register(self.test_user22["name"], self.test_user22["email_address"],
-                                   self.test_user22["password"])
         self.assertEqual(self.user_account.get_user_name(
-            self.user_account.accounts[0]["user_id"]), self.test_user22["name"])
+            self.user_account.accounts[0]["user_id"]), self.test_user23["name"])
 
     def test_get_check_admin(self):
         """
         This method tests the get_check_admin method of the Account class
         :return:
         """
-        self.user_account.register(self.test_user22["name"], self.test_user22["email_address"],
-                                   self.test_user22["password"])
         self.assertEqual(self.user_account.check_admin(self.user_account.accounts[0]["user_id"]), False)
 
     def test_promote_user(self):
@@ -67,11 +57,9 @@ class AccountTestCase(TestBase):
         This method tests the promote_user method of the Account class
         :return:
         """
-        self.user_account.register(self.test_user22["name"], self.test_user22["email_address"],
-                                   self.test_user22["password"])
         self.assertEqual(self.user_account.promote_user(self.user_account.accounts[0]["user_id"]),
-                         {"name": "KingDavid", "email_address": "davidking@gmail.com",
-                          "password": "psaLms198?", "user_id": 1, "admin": True}
+                         {"name": "YossiFunke", "email_address": "tinkacalvin@gmail.com",
+                          "password": "Rejo78!ce", "user_id": 1, "admin": True}
                          )
 
     def test_check_password(self):
@@ -79,9 +67,7 @@ class AccountTestCase(TestBase):
         This method tests the check_password method of the Account class
         :return:
         """
-        self.user_account.register(self.test_user22["name"], self.test_user22["email_address"],
-                                   self.test_user22["password"])
-        self.assertEqual(self.user_account.check_password(self.test_user22["password"]), True)
+        self.assertEqual(self.user_account.check_password(self.test_user23["password"]), True)
 
     def test_validate_email_address(self):
         """
