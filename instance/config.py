d57a62130f2ced0bca32d772fe122ef2edf20d27
calvinpete <tinkacalvin@gmail.com>
2018-10-29 17:45:06 +0300

    Define database for each environment
    [Delivers #161533381]

--- instance/config.py
+++ instance/config.py
@@ -7,12 +7,14 @@ class Config(object):
 class DevelopmentConfig(Config):
     """Configurations for Development"""
     DEBUG = True
+    DATABASE_URL = "postgresql://calvin:310892@127.0.0.1:5432/storemanager"
 
 
 class TestingConfig(Config):
     """Configurations for Testing"""
     TESTING = True
     DEBUG = True
+    DATABASE_URL = "postgresql://calvin:310892@127.0.0.1:5432/storemanagertestdb"
 
 
 class ProductionConfig(Config):
