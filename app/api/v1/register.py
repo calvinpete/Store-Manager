2d2f26706f604c33068af3999bf92e80906cd2bb
calvinpete <tinkacalvin@gmail.com>
2018-10-30 12:19:13 +0300

    Modify the register route
    Refactor code to incoporate validation class and DatabaseConnection class
    
    [Delivers #161533381]

--- app/api/v1/register.py
+++ app/api/v1/register.py
@@ -1,13 +1,15 @@
 from flask import jsonify, request
 from app.api.v1 import app
-from app.api.v1.models.accounts import staff
+from app.api.v1.models.accounts import Account
+from app.api.v1.validator import UserValidator
 from app.api.v1.account import token_required
 
 
-@app.route('/store-manager/api/v1/register', methods=['POST'])
+@app.route('/store-manager/api/v1/auth/signup', methods=['POST'])
 @token_required
 def register_staff(current_user):
-    if not staff.check_admin(current_user):
+
+    if not Account.check_admin(current_user):
         return jsonify({"message": "You do not have administrator access"}), 401
 
     data = request.get_json()
@@ -17,24 +19,31 @@ def register_staff(current_user):
     name = data['name']
     email_address = data['email_address']
     password = data['password']
+    account_type = data['account_type']
 
-    if staff.check_input_type(
+    validate = UserValidator(email_address, password)
+
+    if validate.check_string_input(
             name=name,
             email_address=email_address,
-            password=password):
+            password=password,
+            account_type=account_type):
         return jsonify({"message": "Please enter a string"}), 400
 
-    if staff.check_input_validity(
+    if validate.check_input_validity(
             name=name,
             email_address=email_address,
-            password=password):
+            password=password,
+            account_type=account_type):
         return jsonify({"message": "Values are required"}), 400
 
-    if not staff.validate_email_address(email_address):
+    if not validate.validate_email_address():
         return jsonify({"message": "The email should follow the format of valid emails (johndoe@mail.com)"}), 400
 
-    if staff.check_user(email_address):
-        return jsonify({"message": "Employee already exists"}), 409
+    staff = Account(name, email_address, password, account_type)
+
+    if staff.check_user():
+        return jsonify({"message": "User already exists"}), 409
     else:
-        staff.register(name, email_address, password)
-        return jsonify({"message": "Employee successfully registered"}), 201
+        user = staff.register()
+        return jsonify({"message": "{} has been successfully registered".format(user)}), 201
