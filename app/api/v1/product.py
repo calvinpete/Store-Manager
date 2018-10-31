974965b9c4214cef7d3e548ed2f2f739c03b26f1
calvinpete <tinkacalvin@gmail.com>
2018-10-31 04:26:21 +0300

    Create modify_product route
    [Delivers #161533401]

--- app/api/v1/product.py
+++ app/api/v1/product.py
@@ -1,11 +1,15 @@
 from flask import jsonify, request
 from app.api.v1 import app
+from app.api.v1.account import token_required
 from app.api.v1.validator import UserValidator
 from app.api.v1.models.products import Product
 from app.api.v1.models.accounts import Account
-from app.api.v1.account import token_required
+from app.api.v1.database import DatabaseConnection
 
-#
+
+db = DatabaseConnection()
+
+
 # @app.route('/store-manager/api/v1/category', methods=['POST'])
 # @token_required
 # def create_category(current_user):
@@ -84,3 +88,45 @@ def get_single_product(current_user, product_id):
     #     return jsonify({"message": "Product_id should be a positive integer"}), 404
 
     return Product.get_single_product(product_id)
+
+
+@app.route('/store-manager/api/v1/products/<product_id>', methods=['PUT'])
+@token_required
+def modify_product(current_user, product_id):
+    if Account.check_admin(current_user) != 'admin':
+        return jsonify({"message": "You do not have administrator access"}), 401
+
+    data = request.get_json()
+    if len(data.keys()) != 4:
+        return jsonify({"message": "You should have the product_name, quantity, details and price fields"}), 400
+
+    product_name = data['product_name']
+    quantity = data['quantity']
+    details = data['details']
+    price = data['price']
+
+    if UserValidator.check_string_input(
+            product_name=product_name,
+            details=details):
+        return jsonify({"message": "Please enter a string"}), 400
+
+    if UserValidator.check_integer_input(
+            quantity=quantity,
+            price=price):
+        return jsonify({"message": "Please enter an integer"}), 400
+
+    if UserValidator.check_input_validity(
+            product_name=product_name,
+            details=details):
+        return jsonify({"message": "Values are required"}), 400
+
+    item = Product(product_name, quantity, details, price)
+
+    if db.select_one('products', 'product_id', product_id) is not None:
+        return jsonify({"message": "Product does not exist"}), 404
+
+    item.modify_product(product_id)
+    return jsonify({"message": "Product successfully modified"}), 200
+
+
+
