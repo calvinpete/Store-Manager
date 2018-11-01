47ffbb0f69a19c27a18a6ab083b151ec641640e5
calvinpete <tinkacalvin@gmail.com>
2018-11-02 02:17:14 +0300

    remove unecessary files in .gitignore and update dependancies
    [Delivers #161533381]

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import datetime
 from app.api.v1 import app
 from app.api.v1.database import DatabaseConnection
+from werkzeug.security import generate_password_hash
 
 
 db = DatabaseConnection()
@@ -9,5 +10,5 @@ db = DatabaseConnection()
 if __name__ == "__main__":
     app.run()
     db.create_tables()
-    db.default_admin_setup("store_owner", "admin@gmail.com", "admin00", "admin", datetime.datetime.utcnow(),
-                           datetime.datetime.utcnow())
+    db.default_admin_setup("store_owner", "admin@gmail.com", generate_password_hash("admin00"), "admin",
+                           datetime.datetime.utcnow(), datetime.datetime.utcnow())
