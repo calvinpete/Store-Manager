9de9fd6bfe771432226a96e20e94423ce36f51c7
calvinpete <tinkacalvin@gmail.com>
2018-11-02 07:36:10 +0300

    Run the create table sql queries and insert default admin in the Database
    [Delivers #161533381]

--- run.py
+++ run.py
@@ -1,14 +1,5 @@
-import datetime
 from app.api.v1 import app
-from app.api.v1.database import DatabaseConnection
-from werkzeug.security import generate_password_hash
-
-
-db = DatabaseConnection()
 
 
 if __name__ == "__main__":
     app.run()
-    db.create_tables()
-    db.default_admin_setup("store_owner", "admin@gmail.com", generate_password_hash("admin00"), "admin",
-                           datetime.datetime.utcnow(), datetime.datetime.utcnow())
